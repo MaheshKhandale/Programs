6ba596474c64ef9e53aba820552a358bf1db8da2
Mahesh s Khandale <138959893+MaheshKhandale@users.noreply.github.com>
2023-07-08 11:59:52 +0530

    deleted file modul info.java and added new package
    fdeleted file modul info.java and added new package

---
+++ Programs/src/Programs1/HEllo.java
@@ -0,0 +1,11 @@
+package Programs1;
+
+public class HEllo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("bnbbv");
+
+	}
+
+}
--- Programs/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author MAHESH
- *
- */
-module Programs {
-}
